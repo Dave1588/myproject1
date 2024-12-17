9d2008436f3d6aaaaa36df4409a70463e6b6971d
Dave <davelee1588@gmail.com>
2024-12-17 08:15:26 +0800

    加入圖片, 完成所有圖片板塊

--- myproject/js/switchCategory.js
+++ myproject/js/switchCategory.js
@@ -3,23 +3,46 @@ document.addEventListener('DOMContentLoaded', () => {
     const images = document.querySelectorAll('.image-wrapper .image');
 
     const categoryData = {
-        0: [
-            'https://via.placeholder.com/300x200?text=Category0-1',
-            'https://via.placeholder.com/300x200?text=Category0-2',
-            'https://via.placeholder.com/300x200?text=Category0-3',
-            'https://via.placeholder.com/300x200?text=Category0-4',
-            'https://via.placeholder.com/300x200?text=Category0-5',
-            'https://via.placeholder.com/300x200?text=Category0-6'
+        0: [ // 台灣
+            './img/card0/xi.jpg', 
+            './img/card0/taipei101.jpg', 
+            './img/card0/taroko.avif',
+            './img/card0/sunmoon.avif',
+            './img/card0/train.avif',
+            './img/card0/kendin.avif'  
         ],
-        1: [
-            'https://via.placeholder.com/300x200?text=Category1-1',
-            'https://via.placeholder.com/300x200?text=Category1-2',
-            'https://via.placeholder.com/300x200?text=Category1-3',
-            'https://via.placeholder.com/300x200?text=Category1-4',
-            'https://via.placeholder.com/300x200?text=Category1-5',
-            'https://via.placeholder.com/300x200?text=Category1-6'
+        1: [ // 日本
+            './img/card1/fuji.jpg', // 富士山
+            './img/card1/kyoto.jpg', // 京都清水寺
+            './img/card1/osaka.jpg', // 大阪城
+            './img/card1/disneyland.jpg', // 東京迪士尼
+            './img/card1/deer.avif', // 奈良公園
+            './img/card1/hotspring.avif'  // 箱根溫泉
         ],
-        // 其他類別的圖片資料
+        2: [ // 韓國
+            './img/card2/tower.jpg', // 南山首爾塔
+            './img/card2/gong.avif', // 景福宮
+            './img/card2/world.jpg', // 樂天世界
+            './img/card3/island.avif', // 南怡島
+            './img/card3/myeongdong.jpg', // 明洞
+            './img/card2/jeju.jpg'  // 濟州島
+        ],
+        3: [ // 大陸港澳
+            './img/card3/disney.avif', // 香港迪士尼
+            './img/card3/macou.jpg', // 澳門威尼斯人
+            './img/card3/shanghai.jpg', // 上海外灘
+            './img/card3/beijing.jpg', // 北京長城
+            './img/card3/xiang.avif', // 西安兵馬俑
+            './img/card3/river.jpg'  // 桂林山水
+        ],
+        4: [ // 歐洲
+            './img/card4/ifletower.jpg', // 巴黎鐵塔
+            './img/card4/rome.jpg', // 羅馬競技場
+            './img/card4/venice.jpg', // 威尼斯
+            './img/card4/holan.jpg', // 阿姆斯特丹
+            './img/card4/swiss.jpg', // 少女峰
+            './img/card4/bluswhite.jpg'  // 聖托里尼
+        ]
     };
 
     categoryButtons.forEach(button => {
